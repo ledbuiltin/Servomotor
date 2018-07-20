a7ec5e34e2fba74e8988f1dbc1840c56629e4d5e
David <33182067+ledbuiltin@users.noreply.github.com>
2018-07-20 13:01:32 +0200

    Add files via upload
    Posiciona motor en un angulo dado en el codigo del programa

---
+++ ServoBasico.ino
@@ -0,0 +1,21 @@
+//Programa para llevar un servo a un angulo dado
+//***************************************************************************
+
+#include <Servo.h>  //Libreria del servo
+
+Servo miMotor;    //Creamos objeto de tipo servo al que llamamos miMotor
+int angulo=90;    //Angulo al que llevamos el motor
+
+void setup() {
+
+  miMotor.attach(7);  //Indicamos el pin al que conectamos el motor
+
+}
+ 
+void loop() {
+  
+miMotor.write(angulo);   //Inicamos el motor en posicion central 90º
+ 
+}
+  
+
